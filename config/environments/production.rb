0e07753156420e1453f74f76cf65cb48250d337e
haru <haruki.n0302@gmail.com>
2023-05-08 17:00:35 +0900

    メール周りの設定の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Enjoy_a_Hobby_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { protocol: 'https', host:'live-planning.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
